30ddabf92dab6f666558af672562ff934fedf553
ntrukhanovets <n.trukhanovets@gmail.com>
2018-10-02 20:49:28 +0300

    build folder

---
+++ build/precache-manifest.f5e3a51f0d17156d8b70dbc8750649fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9d4ddb2e4c5edb844d60",
+    "url": "./static/js/main.9d4ddb2e.chunk.js"
+  },
+  {
+    "revision": "3231d75deefaff7887a7",
+    "url": "./static/js/1.3231d75d.chunk.js"
+  },
+  {
+    "revision": "9d4ddb2e4c5edb844d60",
+    "url": "./static/css/main.f5608e67.chunk.css"
+  },
+  {
+    "revision": "bc77beb5cbffcfa9d1956424b0ab9a64",
+    "url": "./index.html"
+  }
+];
